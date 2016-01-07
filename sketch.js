d3a7d42b37e95f5518ca44b474fe11d0349a84b8
aisbleung <bleung@ais.edu.hk>
2016-01-07 14:44:42 +0800

    version 3

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var ocean;
+var beaker;
 
 function preload() {
 
@@ -9,17 +10,26 @@ function setup() {
   // create canvas
   createCanvas(710, 400);
 
+  // load video into computer memory
   ocean = createVideo('videos/Relaxing 1 min Video of A Tropical Beach with Blue Sky White Sand and Palm Tree.mp4');  
   ocean.loop();
   ocean.hide();
+  
+  // load image data
+  img = loadImage("images/beaker.jpg");
 
 }
 
 function draw() {
+
   background(0,125,125);
 
+  // show video on screen
   image(ocean,0,0,710,400);  
 
+
+  // show image
+  image(img,0,0);
 }
 
 
